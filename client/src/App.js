bf957221522e08d685522bbe88b0ab80bb784c7b
Alexandra Seelig <108905816+lolxela@users.noreply.github.com>
2024-03-26 14:14:40 -0400

    added routes to event pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./diffPages/home"
 import CreateEvent from "./diffPages/createEvent";
+import Event from "./diffPages/event";
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
       <Router>
         <div className="navbar">
           <span className="tabs">
-            <Link to="/">Home</Link>
-            <Link to="/createEvent">New Post</Link>
+            <Link to="/">home</Link>
+            <Link to="/createEvent">new event</Link>
           </span>
-          <span className="title">GatorGather</span>
+          <span className="webTitle">GatorGather</span>
         </div>
         <Routes>
           <Route path="/" element={ <Home/> } />
           <Route path="/createEvent" element={ <CreateEvent/> } />
+          <Route path="/event/:id" element={ <Event/> } />
         </Routes>
       </Router>
     </div>
