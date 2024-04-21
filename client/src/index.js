9074148e60bcdcc171b3da22c6f9c5c22e985778
Anthony Davila <anthonydavila90@gmail.com>
2024-04-21 17:17:14 -0400

    Loading screen implementation for when logging in

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { usePromiseTracker } from "react-promise-tracker"
+import { MutatingDots } from "react-loader-spinner";
+
+const LoadingIndicator = props => {
+  const { promiseInProgress } = usePromiseTracker();
+
+  return (
+    promiseInProgress && 
+    <div className="dimScreen">
+      <MutatingDots color="#0021A5" secondaryColor='#FA4616' height="100" width="100" />
+    </div>
+  )
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <LoadingIndicator/>
   </React.StrictMode>
 );
 
